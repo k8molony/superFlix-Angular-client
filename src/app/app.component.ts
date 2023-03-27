e30414f86eab38ac594d371595a3e21b6a564dca
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-03-27 12:01:36 -0400

    6.3 added user registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'superFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  //Open the dialog when signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // Assign the dialog a width
+      width: '280px'
+    });
+  }
 }
