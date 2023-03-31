7d929e8dd3da68fc443e5c42c209fbceb73c658d
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-03-31 11:23:33 -0400

    6.4 add welcome view and movie card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
+// import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+// import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+// import { MatDialog } from '@angular/material/dialog';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,23 @@ import { MatDialog } from '@angular/material/dialog';
 export class AppComponent {
   title = 'superFlix-Angular-client';
 
-  constructor(public dialog: MatDialog) { }
-  //Open the dialog when signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assign the dialog a width
-      width: '280px'
-    });
-  }
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
-    });
-  }
+  // constructor(public dialog: MatDialog) { }
+  // //Open the dialog when signup button is clicked
+  // openUserRegistrationDialog(): void {
+  //   this.dialog.open(UserRegistrationFormComponent, {
+  //     // Assign the dialog a width
+  //     width: '280px'
+  //   });
+  // }
+  // openUserLoginDialog(): void {
+  //   this.dialog.open(UserLoginFormComponent, {
+  //     width: '280px'
+  //   });
+  // }
+
+  // openMoviesDialog(): void {
+  //   this.dialog.open(MovieCardComponent, {
+  //     width: '500px'
+  //   });
+  // }
 }
